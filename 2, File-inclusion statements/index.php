4cee2df5119cba5e7c6dbfbe12d730c84d81984e
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-08-01 11:19:00 +0600

    done

---
+++ 2, File-inclusion statements/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php';
+include 'sidebar.php';
+?>
+
+<section>
+    <h2>মূল বিষয়বস্তু</h2>
+    <p>এখানে তোমার ওয়েবসাইটের মূল কন্টেন্ট থাকবে।</p>
+</section>
+
+<?php
+include 'footer.php';
+?>
